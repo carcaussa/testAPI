48302bff442c8c5d356fe83a22a7ab368232a5f2
Omar Servin <edgar.servin@softtek.com>
2017-09-15 09:55:04 -0500

    3

--- service.js
+++ service.js
@@ -67,7 +67,7 @@ let server = http.createServer( (req, res) => {
 });
 
 
-server.listen(process.env.PORT || process.env.NODE_PORT || srv_port, process.env.NODE_IP || 'walmartapi.azurewebsites.net', () => {
-  console.log(`Application worker ${process.pid} started on port ${srv_port} ...`);
+// server.listen(process.env.PORT || process.env.NODE_PORT || srv_port, process.env.NODE_IP || 'walmartapi.azurewebsites.net', () => {
+//   console.log(`Application worker ${process.pid} started on port ${srv_port} ...`);
 
-});
+// });
